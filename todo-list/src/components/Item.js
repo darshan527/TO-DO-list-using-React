16fde6409dbef89cd0827e1c0560ca2bdd60abfd
darshan527 <darshan.hosamani6@gmail.com>
2021-05-25 00:38:21 +0530

    item

--- todo-list/src/components/Item.js
+++ todo-list/src/components/Item.js
@@ -2,14 +2,9 @@ import React from 'react'
 
 function Item(props) {
     return (
-        <div className="item">
-            <div className="status">
-                <input type="checkbox"/>
-            </div>
-            <div className="task">
+        <div className="task">
                 <h1>{props.item.title}</h1>
                 <h2>{props.item.description}</h2>
-            </div>
         </div>
     )
 }
