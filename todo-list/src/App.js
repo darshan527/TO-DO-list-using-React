57a53bcb8de321e58968a6bd4cc840b0d569b1ad
darshan527 <darshan.hosamani6@gmail.com>
2021-05-24 23:39:48 +0530

    Adder Header

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Item from './components/Item'
+import Header from './components/Header'
 
 function App(){
     return (
         <div>
+            <Header />
             <Item item={{title:"React Project", description:"Complete todo app"}} />
         </div>
     )
