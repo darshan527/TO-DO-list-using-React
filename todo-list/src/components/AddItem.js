21cead142144213750da00d2e0aecd2eb5e0f21c
darshan527 <darshan.hosamani6@gmail.com>
2021-05-26 22:48:14 +0530

    Add item button

--- todo-list/src/components/AddItem.js
+++ todo-list/src/components/AddItem.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import Item from './Item'
 class AddItem extends React.Component {
+    show = false
+    setShow(e){
+        this.show = true
+    }
     render() {
+        <div className="addItem">
+            <input type="button" onClick={e => this.setShow(e)}/>
+        </div>
         return <Item item={{title:"addcheck", description:"Add check description"}} />
     }
 }
