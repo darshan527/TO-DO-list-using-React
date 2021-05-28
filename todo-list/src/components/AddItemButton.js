ba1667d674b6ca514897bab47f190313e843bdf3
darshan527 <darshan.hosamani6@gmail.com>
2021-05-28 23:49:17 +0530

    function to Class

--- todo-list/src/components/AddItemButton.js
+++ todo-list/src/components/AddItemButton.js
@@ -1,3 +1,25 @@
 import React from 'react'
+import AddItem from './AddItem'
+import App from '../App'
 
-// export default function AddItemButton()
+export default class AddItemButton extends React.Component {
+
+     constructor(){
+        super()
+        this.ad = this.ad.bind(this)
+     }
+
+    ad(){
+        const t = prompt("Enter the title");
+        const d = prompt("Enter the Description");
+        App.renderTasks(<AddItem item={{title:t, description:d}} />)
+    }
+
+    render(){
+        return (
+            <div className="addItem">
+                <button onClick={() => this.ad()}>Add Item</button>
+            </div>
+        )
+    }
+}
