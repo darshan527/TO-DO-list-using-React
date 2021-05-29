b8c4ee36e6e6e3dab9508102acadda7629d0b2c9
darshan527 <darshan.hosamani6@gmail.com>
2021-05-29 22:27:09 +0530

    wip

--- todo-list/src/components/AddItem.js
+++ todo-list/src/components/AddItem.js
@@ -1,16 +1,30 @@
 import React from 'react'
 import Item from './Item'
-class AddItem extends React.Component {
-    show = false
-    setShow(e){
-        this.show = true
-    }
-    render() {
-        <div className="addItem">
-            <input type="button" onClick={e => this.setShow(e)}/>
-        </div>
-        return <Item item={{title:"addcheck", description:"Add check description"}} />
-    }
+// class AddItem extends React.Component {
+//     show = false
+//     setShow(e){
+//         this.show = true
+//     }
+//     render() {
+//         if (this.show){
+//             console.log("activveeeve")
+//             return (
+//                 <Item item={{title:"addcheckeeeee", description:"Add check description"}} />
+//             )
+//         }
+//         else {
+//             return (
+//                 <div className="addItem">
+//                     <input type="button" name="AddItem" onClick={e => this.setShow(e)}/>
+//                 </div>
+//             )
+//         }
+//     }
+// }
+function AddItem(){
+    return (
+        <Item item={{title:"addcheck", description:"Add check description"}} />
+    )
 }
 
 export default AddItem
