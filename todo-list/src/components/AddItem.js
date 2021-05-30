093075c3148ac86c4bfe09b15fbc82a95aef2743
darshan527 <darshan.hosamani6@gmail.com>
2021-05-30 22:34:11 +0530

    wip

--- todo-list/src/components/AddItem.js
+++ todo-list/src/components/AddItem.js
@@ -1,26 +1,39 @@
 import React from 'react'
 import Item from './Item'
-// class AddItem extends React.Component {
-//     show = false
-//     setShow(e){
-//         this.show = true
-//     }
-//     render() {
-//         if (this.show){
-//             console.log("activveeeve")
-//             return (
-//                 <Item item={{title:"addcheckeeeee", description:"Add check description"}} />
-//             )
-//         }
-//         else {
-//             return (
-//                 <div className="addItem">
-//                     <input type="button" name="AddItem" onClick={e => this.setShow(e)}/>
-//                 </div>
-//             )
-//         }
-//     }
-// }
+class AddItem extends React.Component {
+    // show = false
+    // setShow(e){
+    //     this.show = true
+    // }
+    constructor(props){
+        super(props)
+        this.state = {
+            title : props.item.title,
+            description : props.item.description
+        }
+    }
+
+
+    createItem(){
+
+    }
+
+    render() {
+        if (this.show){
+            console.log("activveeeve")
+            return (
+                <Item item={{title:"addcheckeeeee", description:"Add check description"}} />
+            )
+        }
+        else {
+            return (
+                <div className="addItem">
+                    <input type="button" name="AddItem" onClick={e => this.setShow(e)}/>
+                </div>
+            )
+        }
+    }
+}
 function AddItem(){
     return (
         <Item item={{title:"addcheck", description:"Add check description"}} />
