17b41386dedda0c0d1b6c4eb1bfd0c2518c9d67e
darshan527 <darshan.hosamani6@gmail.com>
2021-06-12 23:33:17 +0530

    wip

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -4,7 +4,6 @@ import Item from './components/Item'
 
 function App(){
     const [tasksList, setTask] = useState([])
-    // const [count , setCount] = useState(0)
     const [renderList, setRenderList] = useState([])
 
     function renderTasks(){
