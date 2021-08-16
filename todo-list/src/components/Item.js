17de2c97e044256c0796ad9bd842073ca9e196ac
darshan527 <darshan.hosamani6@gmail.com>
2021-08-16 23:40:57 +0530

    Converted to Functional Component

--- todo-list/src/components/Item.js
+++ todo-list/src/components/Item.js
@@ -1,31 +1,12 @@
 import React from 'react'
 
-class Item extends React.Component {
-
-    constructor(props){
-        super(props)
-        this.state = {
-                title : props.item.title,
-                description : props.item.description
-        }
-        this.changeDesc = this.changeDesc.bind(this)
-    }
-
-
-    changeDesc = () => {
-        console.log(this.state.description)
-        this.setState({description: "changedit"})
-    }
-
-    render(){
-        return (
-            <div className="task" >
-                    <h1>{this.state.title}</h1>
-                    <h2>{this.state.description}</h2>
-            </div>
-        )
-    }
-
+const Item = (props) => {
+    const task = props.item
+    return (
+        <div className="task" onClick={() => props.removeItem(task.id)} >
+            <h1>{task.title}</h1>
+            <h2>{task.description}</h2>
+        </div>
+    )
 }
-
 export default Item
